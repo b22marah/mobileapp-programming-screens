113a62c736a75ef31818eaadb8b9a404bc59e4c8
Marcus Ahlberg <b22marah@student.his.se>
2023-04-26 16:23:41 +0200

    Small tweets with the value given through the intent / extras.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra("name", "Marcus");
+                intent.putExtra("name", "Yippie! We did it!!!");
                 startActivity(intent);
             }
         });
